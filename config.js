588707c938001d947793716922184afd8bb3d8fa
Emily Kauffman <emilykauffman@harvie.farm>
2021-01-11 03:51:20 -0500

    limited set

--- config.js
+++ config.js
@@ -34,16 +34,16 @@ const config = {
       '/week1',
       '/week2',
       '/week3',
-      '/week4',
-      '/week5',
-      '/week6',
-      '/week7',
-      '/week8',
-      '/week9',
-      '/week10',
-      '/week11',
-      '/week12',
-      '/week13'
+      // '/week4',
+      // '/week5',
+      // '/week6',
+      // '/week7',
+      // '/week8',
+      // '/week9',
+      // '/week10',
+      // '/week11',
+      // '/week12',
+      // '/week13'
     ],
     collapsedNav: [
       // '/codeblock', // add trailing slash if enabled above
